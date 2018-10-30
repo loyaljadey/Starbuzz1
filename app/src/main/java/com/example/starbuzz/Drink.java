30368e54c37b563c95fc4fb4e2bfa6bc2ac4fa20
CharlesLiu02 <roomcool02@gmail.com>
2018-10-30 09:34:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/starbuzz/Drink.java
@@ -0,0 +1,35 @@
+package com.example.starbuzz;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Drink[] drinks = {
+            new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
+            new Drink("Cappuccinco", "Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.latte)
+    };
+
+    public Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+}
