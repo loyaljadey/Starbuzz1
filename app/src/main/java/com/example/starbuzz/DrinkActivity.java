4e20d75cb84fc09d08ced671b46adb4254c1e241
CharlesLiu02 <roomcool02@gmail.com>
2018-11-01 09:14:53 -0700

    added list functionality to top level activity and drink category activity displays drink information after user clicks on drink from list in drink category activity

---
+++ app/src/main/java/com/example/starbuzz/DrinkActivity.java
@@ -0,0 +1,29 @@
+package com.example.starbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DrinkActivity extends AppCompatActivity {
+    public static final String EXTRA_DRINKID = "drinkid";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        TextView name = findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = findViewById(R.id.description);
+        description.setText(drink.getDescription());
+
+        ImageView photo = findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+    }
+}
--- app/src/main/java/com/example/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/starbuzz/TopLevelActivity.java
@@ -1,7 +1,11 @@
 package com.example.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if(position == 0){
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listview = (ListView)findViewById(R.id.list_options);
+        listview.setOnItemClickListener(itemClickListener);
     }
 }
